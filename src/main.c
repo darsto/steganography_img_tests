73d9110608f3109fe613632786deaf33ffe26aad
dar <darek.stojaczyk@gmail.com>
2017-04-01 22:12:33 +0200

    Added copyright header to main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 Dariusz Stojaczyk. All Rights Reserved.
+ * The following source code is released under an MIT-style license,
+ * that can be found in the LICENSE file.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
